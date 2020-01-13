a11738c72ff7fbaf77403c72042881e8a146d07f
sergio <rocmen@yandex.ru>
2020-01-13 05:24:25 +0500

    add callibri

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const scripts = [...document.getElementsByTagName("script")],
-        reg = [/jivosite/i, /callibri_soc/i],
+        reg = [/jivosite/i, /callibri/i],
         reg0 = /callback.onlinepbx/i;
 
 
@@ -8,18 +8,29 @@ window.onload = () => {
         const find = reg.find(elem => elem.test(script.outerHTML));
         const findPbx = reg0.test(script.outerHTML);
 
-        //remove jdiv
+
         if (find != undefined) {
             const idInterval = setInterval(() => {
 
+                //remove jdiv
                 let jdiv = [...document.getElementsByTagName("jdiv")];
                 if (jdiv.length > 0) {
                     jdiv.forEach(element => {
                         element.remove();
-
+                        console.log("remove jdiv");
                     })
                     clearInterval(idInterval);
                 }
+
+                //remove callibri
+                let callibri = document.getElementById("callibri_soc");
+                if (callibri != undefined) {
+                    callibri.remove();
+                    clearInterval(idInterval);
+                    console.log("callibri");
+                }
+
+                //clearInterval(idInterval);
             }, 100)
         }
 
@@ -28,68 +39,5 @@ window.onload = () => {
             script.outerHTML = "";
 
         }
-
     })
 }
-
-
-
-
-// let jdiv = [...document.getElementsByTagName("jdiv")];
-// console.log(jdiv);
-// jdiv.forEach((element) => {
-//     element.remove();
-// })
-
-
-
-
-
-
-// for (let i = 0; i < scripts.length; i++) {
-//     if (reg0.test(scripts[i].outerHTML)) {
-//         scripts[i].outerHTML = "";
-
-//     }
-// }
-// console.log(scripts);
-
-
-
-
-// let scripts = document.getElementsByTagName("script");
-// let reg = /jivosite/i;
-// let reg0 = /callback.onlinepbx/i;
-
-
-// for (let i = 0; i < scripts.length; i++) {
-//     if (reg.test(scripts[i].outerHTML)) {
-//         console.log("here");
-//         scripts[i].outerHTML = "";
-
-//     }
-// }
-// console.log(scripts);
-
-// let prm = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         let number = 1;
-//     }, 2000)
-//     resolve(number);
-
-// })
-
-// prm.then(() => {
-//     console.log("done", number)
-// })
-
-
-// [...document.getElementsByTagName('*')].forEach(e => {
-//     e.className && e.className.match(/onpbxcb/) && e.className.match(/onpbxcb/)[0] != null && e.remove()
-// })
-
-
-
-// document.addEventListener('readystatechange', (e) => {
-//     console.log(document.readyState)
-// })
